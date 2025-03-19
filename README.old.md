c540d643cea9a7a0a9c3edf1003eb224a0714610
Huseyin <huseyineskan01@gmail.com>
2025-03-19 01:08:03 +0100

    it works very well with local quotes.

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# Random-Quote-Machine
+
+Random Quote Machine
---
+++ src/App.js
@@ -0,0 +1,76 @@
+import "./App.css";
+import React, { useState } from "react";
+import { FaXTwitter, FaFacebookF } from "react-icons/fa6";
+
+const quotes = [
+  {
+    quote: "The will of man is his happiness.",
+    author: "Friedrich Schiller",
+    category: "happiness",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+    category: "work",
+  },
+  {
+    quote: "The only thing we have to fear is fear itself.",
+    author: "Franklin D. Roosevelt",
+    category: "fear",
+  },
+  {
+    quote: "The only true wisdom is in knowing you know nothing.",
+    author: "Socrates",
+    category: "wisdom",
+  },
+  {
+    quote: "The only thing necessary for the triumph of evil",
+    author: "Edmund Burke",
+    category: "evil",
+  },
+];
+
+function Quote() {
+  const [currentQuote, setCurrentQuote] = useState(
+    quotes[Math.floor(Math.random() * quotes.length)]
+  );
+
+  function newQuote() {
+    setCurrentQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  }
+
+  return (
+    <div id="quote-box">
+      <p id="text">
+        <span>"</span>
+        {currentQuote.quote}
+      </p>
+      <p id="author">- {currentQuote.author}</p>
+      <div className="row">
+        <div className="socials">
+          <a id="tweet-quote" href="https://twitter.com/intent/tweet">
+            <FaXTwitter />
+          </a>
+          <a id="tweet-quote" href="https://facebook.com/">
+            <FaFacebookF />
+          </a>
+        </div>
+        <button id="new-quote" onClick={newQuote}>
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Quote />
+      </header>
+    </div>
+  );
+}
+
+export default App;
