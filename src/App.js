947c3e41b20160b16d91ec5bc34c5631af29adcc
Huseyin <huseyineskan01@gmail.com>
2025-03-20 20:51:42 +0100

    added random color changing every new quote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { MdFormatQuote } from "react-icons/md";
 import { FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
 
 const quotes = [
@@ -34,51 +35,75 @@ function Quote() {
   const [currentQuote, setCurrentQuote] = useState(
     quotes[Math.floor(Math.random() * quotes.length)]
   );
+  const [color, setColor] = useState("");
 
   function newQuote() {
     setCurrentQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+    newColor();
   }
 
+  function newColor() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
+    console.log(randomColor);
+  }
+
+  useEffect(() => {
+    newColor();
+    document.body.style.backgroundColor = "green";
+    return () => {
+      document.body.style.backgroundColor = "transparent";
+    };
+  }, []);
+
   return (
-    <div id="quote-box">
-      <p id="text">
-        <span>"</span>
-        {currentQuote.quote}
-      </p>
-      <p id="author">- {currentQuote.author}</p>
-      <div className="row">
-        <div className="socials">
-          <a
-            id="tweet-quote"
-            target="_blank"
-            href={`https://twitter.com/intent/tweet?text=${currentQuote.quote} - ${currentQuote.author}`}
-          >
-            <FaXTwitter />
-          </a>
-          <a
-            id="linkedin-quote"
-            target="_blank"
-            href={`https://www.linkedin.com/shareArticle?mini=true&text="${currentQuote.quote}" - ${currentQuote.author}`}
-          >
-            <FaLinkedinIn />
-          </a>
+    <div className="App">
+      <header
+        className="App-header"
+        style={{ backgroundColor: color, transition: "all 0.5s" }}
+      >
+        <div id="quote-box">
+          <p id="text">
+            <MdFormatQuote />
+            {currentQuote.quote}
+            <MdFormatQuote />
+          </p>
+          <p id="author">- {currentQuote.author}</p>
+          <div className="row">
+            <div className="socials">
+              <a
+                id="tweet-quote"
+                target="_blank"
+                rel="noreferrer"
+                href={`https://twitter.com/intent/tweet?text=${currentQuote.quote} - ${currentQuote.author}`}
+              >
+                <FaXTwitter style={{ color: color }} />
+              </a>
+              <a
+                id="linkedin-quote"
+                target="_blank"
+                rel="noreferrer"
+                href={`https://www.linkedin.com/shareArticle?mini=true&text="${currentQuote.quote}" - ${currentQuote.author}`}
+              >
+                <FaLinkedinIn style={{ color: color }} />
+              </a>
+            </div>
+            <button
+              id="new-quote"
+              style={{ backgroundColor: color, transition: "all 0.5s" }}
+              onClick={newQuote}
+            >
+              New Quote
+            </button>
+          </div>
         </div>
-        <button id="new-quote" onClick={newQuote}>
-          New Quote
-        </button>
-      </div>
+      </header>
     </div>
   );
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Quote />
-      </header>
-    </div>
-  );
+  return <Quote />;
 }
 
 export default App;
